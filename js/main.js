50ee353a9ea08995e5c086c4e78a86edc6a6a6a1
Saveleva Kristina <savelevakristina1995@yandex.ru>
2022-01-19 23:22:57 +0300

    new section

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(function(){
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false
-    })
+    });
+
+    $('select').styler();
 
 
 
