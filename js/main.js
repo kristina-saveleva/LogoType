6f4e5c2df81e638769e58d84811612c453def11b
Saveleva Kristina <savelevakristina1995@yandex.ru>
2022-01-28 17:31:57 +0300

    finished media

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function(){
 
+    $('a[href*=#]').bind('click', function(e) {
+        e.preventDefault();
+        
+        var target = $(this).attr("href");
+        
+        $('html, body').stop().animate({ scrollTop: $(target).offset().top}, 500, function() {
+        location.hash = target;
+        });
+        
+        return false;
+    });
+
     $('.slider__inner, .news__slider-inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
@@ -11,4 +23,6 @@ $(function(){
     $('.header__btn-menu').on('click', function(){
         $('.menu ul').slideToggle();
     });
+
 });
+
