8355b523e9b62ead3c8aea7cadd61eead0e50d9c
Saveleva Kristina <savelevakristina1995@yandex.ru>
2022-01-28 18:30:39 +0300

    added footer

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 $(function(){
 
-    $('a[href*=#]').bind('click', function(e) {
-        e.preventDefault();
-        
-        var target = $(this).attr("href");
-        
-        $('html, body').stop().animate({ scrollTop: $(target).offset().top}, 500, function() {
-        location.hash = target;
-        });
-        
-        return false;
-    });
 
     $('.slider__inner, .news__slider-inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
